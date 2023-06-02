ee8294ee2575dbe3ed89f714359ad80e97a80e5f
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-06-02 15:44:53 -0400

    Bumps version to 0.0.2

--- prostate_nomograms/__init__.py
+++ prostate_nomograms/__init__.py
@@ -2,3 +2,12 @@ from .capra import CapraNomogram
 from .custom import CustomNomogram
 from .mskcc import MskccPreRadicalProstatectomyNomogram
 from .enum import ClassificationOutcome, SurvivalOutcome
+
+__author__ = "Maxence Larose"
+__version__ = "0.0.2"
+__copyright__ = "Copyright 2023, Maxence Larose"
+__credits__ = ["Maxence Larose"]
+__license__ = "Apache License 2.0"
+__maintainer__ = "Maxence Larose"
+__email__ = "maxence.larose.1@ulaval.ca"
+__status__ = "Production"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="prostate-nomograms",
-    version="0.0.1",
+    version="0.0.2",
     author="Maxence Larose",
     author_email="maxence.larose.1@ulaval.ca",
     description="Prediction tools based on existing prostate cancer nomograms.",
