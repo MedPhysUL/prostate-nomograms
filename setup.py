fcc6cfeb25d4aa17f6dab469bb138e7ee01788e9
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-06-02 15:34:00 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+setuptools.setup(
+    name="prostate-nomograms",
+    version="0.0.1",
+    author="Maxence Larose",
+    author_email="maxence.larose.1@ulaval.ca",
+    description="Prediction tools based on existing prostate cancer nomograms.",
+    long_description=open('README.md', "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MaxenceLarose/prostate-nomograms",
+    license="Apache License 2.0",
+    keywords='cancer medical nomogram prediction prostate python3',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "openpyxl",
+        "pandas",
+        "requests",
+        "scikit-learn",
+        "scikit-survival",
+        "scipy"
+    ],
+)
