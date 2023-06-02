f065cf987d0f7a345596c3d53aead0ac6867ae97
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-06-02 16:02:20 -0400

    Bumps version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="prostate-nomograms",
-    version="0.0.2",
+    version="0.0.3",
     author="Maxence Larose",
     author_email="maxence.larose.1@ulaval.ca",
     description="Prediction tools based on existing prostate cancer nomograms.",
